93c4cfffe4b74ccb2943bf9a14fa335aef659af4
Jill Platts <jillplatts@gmail.com>
2020-12-10 13:34:05 -0500

    Add files via upload

---
+++ get_jupyter_notebook_dependencies.py
@@ -0,0 +1,24 @@
+import pkg_resources
+import types
+def get_imports():
+    for name, val in globals().items():
+        if isinstance(val, types.ModuleType):
+            name = val.__name__.split(".")[0]
+
+        elif isinstance(val, type):
+            name = val.__module__.split(".")[0]
+        poorly_named_packages = {
+            "sklearn": "scikit-learn"
+        }
+        if name in poorly_named_packages.keys():
+            name = poorly_named_packages[name]
+
+        yield name
+imports = list(set(get_imports()))
+requirements = []
+for m in pkg_resources.working_set:
+    if m.project_name in imports and m.project_name!="pip":
+        requirements.append((m.project_name, m.version))
+
+for r in requirements:
+    print("{}: {}".format(*r))
